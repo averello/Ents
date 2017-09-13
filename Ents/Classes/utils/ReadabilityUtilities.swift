65b2e26e899338ccfe4187de38e6fe5e1e9b61f9
Georges Boumis <developer.george.boumis@gmail.com>
2017-09-13 14:09:37 +0300

    obsoleted with(::) function for value types in swift 4.0

--- Ents/Classes/utils/ReadabilityUtilities.swift
+++ Ents/Classes/utils/ReadabilityUtilities.swift
@@ -13,8 +13,17 @@ public func by<Element>(_ element: Element) -> Element {
     return element
 }
 
+@available(swift 3.1)
+@available(swift, deprecated: 3.2)
+@available(swift, obsoleted: 4.0, message: "doesn't work well with refererces types.")
 public func with<Element>(_ o: Element, _ block: (inout Element) -> Void) -> Element {
     var e = o
     block(&e)
+    return e
+}
+
+@available(swift, introduced: 4.0)
+public func with<Element>(_ e: Element, _ block: (Element) -> Void) -> Element where Element: AnyObject {
+    block(e)
     return e
 }
