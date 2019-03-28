da98f10a0842709b23316f52d6dc67914dff84ab
Georges Boumis <developer.george.boumis@gmail.com>
2019-03-28 17:12:27 +0200

    3.2.5 Introduce md5hash & hex encoding for String & Data

---
+++ Ents/Classes/extensions/types/DataExtensions.swift
@@ -0,0 +1,34 @@
+//
+//  DataExtensions.swift
+//  Ents
+//
+//  Created by Georges Boumis on 28/03/2019.
+//
+
+import Foundation
+
+
+#if canImport(CommonCrypto)
+import CommonCrypto
+
+extension Data {
+    
+    var md5Hash: Data? {
+        if self.isEmpty {
+            return nil
+        }
+        return self.withUnsafeBytes { (pointer: UnsafePointer<UInt8>) -> Data in
+            let length = CC_MD5_DIGEST_LENGTH
+            var result = [UInt8](repeating: 0, count: Int(length))
+            CC_MD5(pointer, CC_LONG(length), &result)
+            return Data(bytes: result)
+        }
+    }
+    
+    var hexEncoded: String {
+        return self.withUnsafeBytes { (buffer: UnsafeRawBufferPointer) -> String in
+            return buffer.map({ String(format: "%02x", $0) }).joined()
+        }
+    }
+}
+#endif
---
+++ Ents/Classes/extensions/types/StringExtensions.swift
@@ -0,0 +1,23 @@
+//
+//  StringExtensions.swift
+//  Ents
+//
+//  Created by Georges Boumis on 28/03/2019.
+//
+
+import Foundation
+
+#if canImport(CommonCrypto)
+import CommonCrypto
+
+extension String {
+    
+    var md5Hash: Data? {
+        if self.isEmpty {
+            return nil
+        }
+        guard let data = self.data(using: String.Encoding.utf8) else { return nil }
+        return data.md5Hash
+    }
+}
+#endif
